743a2c0cfb71d8be4fd929f127b1eba426aa1ed5
menglu <251230950@qq.com>
2021-08-05 16:00:11 +0800

    Initial commit

---
+++ src/main/java/com/example/springboot/Animal.java
@@ -0,0 +1,30 @@
+package com.example.springboot;
+
+public class Animal {
+
+  public String name;
+
+
+  public int age;
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public Animal(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+}
